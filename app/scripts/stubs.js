a53b66eab672450f589be440cfd520fb0e6b02ee
Wassim CHEGHAM <wchegham@sii.fr>
2015-03-14 11:43:23 +0100

    disable stubs

--- app/scripts/stubs.js
+++ app/scripts/stubs.js
@@ -7,20 +7,20 @@
  */
 RPG.run(function() {
   'use strict';
-  var ps = RPG.Factory.pubsub();
-  setTimeout(function() {
-    ps.publish(RPG.topics.SUB_PLAYER_DIED, {
-      "@c": "string",
-      "id": "string",
-      "life": {
-        "current": 0,
-        "max": 0
-      },
-      "playerInfo": {
-        "name": "string",
-        "avatar": "string"
-      },
-      "states": "string"
-    });
-  }, 5000);
+  // var ps = RPG.Factory.pubsub();
+  // setTimeout(function() {
+  //   ps.publish(RPG.topics.SUB_PLAYER_DIED, {
+  //     "@c": "string",
+  //     "id": "string",
+  //     "life": {
+  //       "current": 0,
+  //       "max": 0
+  //     },
+  //     "playerInfo": {
+  //       "name": "string",
+  //       "avatar": "string"
+  //     },
+  //     "states": "string"
+  //   });
+  // }, 10000);
 });
