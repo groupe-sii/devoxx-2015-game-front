1f65c7f06cc327afeb1a6ecebe922f6291005773
Wassim CHEGHAM <wchegham@sii.fr>
2015-03-14 11:31:26 +0100

    update Gfx engine with animation support

--- app/scripts/Utils/Gfx.js
+++ app/scripts/Utils/Gfx.js
@@ -13,9 +13,8 @@ RPG.module('Gfx', function() {
     this.ai = AI;
     this.name = 'Gfx';
     this.gridSize = 10;
-    this.itemElement = 'rpg-entity';
+    this.entityTag = 'rpg-entity';
     this.playerEntity = null;
-
     this.joystick = document.querySelector('#rpg-joystick');
     this.boardContainer = document.querySelector('#rpg-grid');
     this.buttons = document.querySelector('#action-buttons');
@@ -33,35 +32,30 @@ RPG.module('Gfx', function() {
     this.build();
   }
   Gfx.prototype.loop = function() {
-
     this.pubsub.subscribe('/gfx/cell/click', function( /*topic, data*/ ) {});
     this.pubsub.subscribe('/gfx/item/place', function() {});
     this.pubsub.subscribe(RPG.topics.SUB_PLAYER_JOINED, function(topic, data) {
       this.playerEntity = this.placeEntity(data, 'player');
-      
       var enemy = null;
       var nbEnemy = 5;
       for (var i = nbEnemy - 1; i >= 0; i--) {
+        //@todo create enemies accordingly to server
         enemy = this.placeEntity({
           player: {
             playerInfo: {
               avatar: 'images/players-sprites/dvl1_fr1.gif.png',
-              name: 'Enemy '+i,
+              name: 'Enemy ' + i,
               life: (Math.random() * 100)
             }
           },
-          newCell: this.generatePosition()
+          newCell: this.generateRandomPosition()
         }, 'enemy');
       };
-      
     }.bind(this));
-    
     this.pubsub.subscribe(RPG.topics.SUB_PLAYER_LEFT, function(topic, data) {
       this.removePlayer(data.oldCell);
     }.bind(this));
-    
     this.pubsub.subscribe(RPG.topics.SUB_OTHER_JOINED, function(topic, data) {});
-    
     this.pubsub.subscribe(RPG.topics.SUB_PLAYER_MOVED, function(topic, data) {
       this.playerEntity.setAttribute('current-position-x', data.newCell.x);
       this.playerEntity.setAttribute('current-position-y', data.newCell.y);
@@ -69,13 +63,17 @@ RPG.module('Gfx', function() {
       this.playerEntity.setAttribute('previous-position-y', data.oldCell.y);
       this.moveTo();
     }.bind(this));
-
+    this.pubsub.subscribe(RPG.topics.SUB_PLAYER_DIED, function(topic, data) {
+      var entity = this.playerEntity; // this.findEntity(data.newCell);
+      if(entity){
+        entity.explode();
+      }
+    }.bind(this));
     window.addEventListener('beforeunload', function() {
       this.pubsub.publish('/transport/close');
     }.bind(this));
-
   };
-  Gfx.prototype.generatePosition = function() {
+  Gfx.prototype.generateRandomPosition = function() {
     return {
       x: ((Math.random() * this.gridSize) | 0),
       y: ((Math.random() * this.gridSize) | 0)
@@ -87,7 +85,7 @@ RPG.module('Gfx', function() {
       grid += '<tr>';
       for (var j = 0; j < this.gridSize; j += 1) {
         grid += '<td data-y="' + i + '" data-x="' + j + '">';
-        grid += '&middot;';
+        grid += '';
         grid += '</td>';
       }
       grid += '</tr>';
@@ -135,20 +133,20 @@ RPG.module('Gfx', function() {
     var btnQuit = document.querySelector('[data-action="leave"]');
     this.pubsub.subscribe(RPG.topics.SUB_PLAYER_JOINED, function() {
       board.classList.remove('blur');
-    	btnQuit.classList.remove('hidden');
-		  menuContainer.classList.add('move-top');
+      btnQuit.classList.remove('hidden');
+      menuContainer.classList.add('move-top');
     });
-    this.upperButtons.on('click', function(e){
-    	e.preventDefault();
+    this.upperButtons.on('click', function(e) {
+      e.preventDefault();
       var action = e.target;
       if (action && action.nodeName === 'BUTTON') {
         switch (action.dataset.action) {
           case 'leave':
-          	btnQuit.classList.add('hidden');
-						menuContainer.classList.remove('move-top');
-						board.classList.add('blur');
+            btnQuit.classList.add('hidden');
+            menuContainer.classList.remove('move-top');
+            board.classList.add('blur');
             this.pubsub.publish(RPG.topics.PUB_PLAYER_LEAVE);
-          break;
+            break;
         }
       }
     });
@@ -156,21 +154,20 @@ RPG.module('Gfx', function() {
       e.preventDefault();
       var action = e.target;
       if (action && action.nodeName === 'IMG') {
-      	action.parentElement.querySelector('.selected').classList.remove('selected');
-      	action.classList.add('selected');
-      	this.avatars.selected = action.src;
+        action.parentElement.querySelector('.selected').classList.remove('selected');
+        action.classList.add('selected');
+        this.avatars.selected = action.src;
       }
     });
     var btnJoin = this.buttons.querySelector('[data-action="join"]');
-    this.username.on('keyup', function(e){
-    	var value = e.target.value;
-    	if(value === ''){
-    		btnJoin.setAttribute('disabled', true);
-    	}
-    	else {
-    		btnJoin.removeAttribute('disabled');
-    		this.username.value = value;
-    	}
+    this.username.on('keyup', function(e) {
+      var value = e.target.value;
+      if (value === '') {
+        btnJoin.setAttribute('disabled', true);
+      } else {
+        btnJoin.removeAttribute('disabled');
+        this.username.value = value;
+      }
     });
     document.addEventListener('keydown', function(e) {
       var topic = '';
@@ -207,7 +204,7 @@ RPG.module('Gfx', function() {
     });
   };
   Gfx.prototype.createEntity = function(obj, type) {
-    var entity = document.createElement(this.itemElement);
+    var entity = document.createElement(this.entityTag);
     entity.setAttribute('type', type);
     entity.setAttribute('life', obj.player.playerInfo.life);
     entity.setAttribute('name', obj.player.playerInfo.name);
@@ -216,7 +213,7 @@ RPG.module('Gfx', function() {
     entity.setAttribute('current-position-y', obj.newCell.y);
     entity.setAttribute('previous-position-x', (obj.oldCell && obj.oldCell.x) || 0);
     entity.setAttribute('previous-position-y', (obj.oldCell && obj.oldCell.y) || 0);
-    entity.addEventListener('click', function(e){
+    entity.addEventListener('click', function(e) {
       this.selectEntity(e.target);
     }.bind(this), false);
     return entity;
@@ -228,17 +225,9 @@ RPG.module('Gfx', function() {
     this.pubsub.publish('/gfx/item/placed', entity);
     return entity;
   };
-  // Gfx.prototype.transposePosition = function(item, cell) {
-  //   if(item){
-  //     item.transpose({
-  //       top: cell.offsetTop,
-  //       left: cell.offsetLeft
-  //     });
-  //   }
-  // };
-  Gfx.prototype.selectEntity = function(entity){
-    switch(entity.type){
-      case 'player': 
+  Gfx.prototype.selectEntity = function(entity) {
+    switch (entity.type) {
+      case 'player':
         this.selectPlayer(entity);
         break;
       case 'enemy':
@@ -248,7 +237,7 @@ RPG.module('Gfx', function() {
   };
   Gfx.prototype.selectPlayer = function(player) {
     var container, life, name, avatar;
-    if(player){
+    if (player) {
       container = document.querySelector('#rpg-selected-player');
       life = '<span class="rpg-life"><i style="width:' + player.getAttribute('life') + '%;"></i></span>';
       name = '<span class="rpg-name">' + player.getAttribute('name') + '</span>';
@@ -263,66 +252,46 @@ RPG.module('Gfx', function() {
     var image = '<img src="' + enemy.getAttribute('avatar') + '" width="32px" height="32px"/>';
     container.innerHTML = image + name + life;
   };
-  Gfx.prototype.moveTo = function(entity){
-
+  Gfx.prototype.moveTo = function(entity) {
     var oldCell, newCell;
-    entity = entity || this.playerEntity;
-
-    if(entity){
-
-      oldCell = this.boardContainer.querySelector('td[data-x="' + entity.getAttribute('previous-position-x') + '"][data-y="' + entity.getAttribute('previous-position-y') + '"]');
-      if(oldCell){
+    entity = entity ||  this.playerEntity;
+    if (entity) {
+      oldCell = this.findCell({
+        x: entity.getAttribute('previous-position-x'),
+        y: entity.getAttribute('previous-position-y')
+      });
+      if (oldCell) {
         oldCell.classList.remove('rpg-occupied');
       }
-      
-      newCell = this.boardContainer.querySelector('td[data-x="' + entity.getAttribute('current-position-x') + '"][data-y="' + entity.getAttribute('current-position-y') + '"]');
-      if(newCell){
+      newCell = this.findCell({
+        x: entity.getAttribute('current-position-x'),
+        y: entity.getAttribute('current-position-y')
+      });
+      if (newCell) {
         newCell.classList.add('rpg-occupied');
       }
-
       entity.style.top = (newCell.offsetTop + 17) + 'px';
       entity.style.left = (newCell.offsetLeft + 53) + 'px';
-
     }
-
   };
-  // Gfx.prototype.isEmptyCell = function(obj) {
-  //   var cell = this.getItem(obj, 'td');
-  //   return !this.isOut(obj) && !cell.classList.contains('rpg-occupied');
-  // };
-  // Gfx.prototype.isOut = function(obj) {
-  //   return obj.x >= this.gridSize || obj.x < 0 || obj.y < 0 || obj.y >= this.gridSize;
-  // };
-  // Gfx.prototype.getFreeNeighbours = function(obj) {
-  //   var cell = this.getItem(obj, 'td');
-  //   return this.ai.getFreeNeighbours(cell.dataset);
-  // };
-  // Gfx.prototype.translate = function(o1, o2) {
-  //   var oldCell = this.getItem(o1, 'td');
-  //   var newCell = this.getItem(o2, 'td');
-  //   var item = this.getPlayer(o1);
-
-  //   if(item){
-  //     oldCell.classList.remove('rpg-occupied');
-  //     newCell.classList.add('rpg-occupied');
-  //     this.transposePosition(item, newCell);
-  //     item.set({
-  //       position: o2
-  //     });
-  //   }
-  // };
   Gfx.prototype.remove = function(obj) {
-    var cell = this.boardContainer.querySelector('td[data-x="' + obj.x + '"][data-y="' + obj.y + '"]');
+    var cell =  this.findCell(obj);
     cell.classList.remove('rpg-occupied');
-    cell.innerHTML = '&middot;';
+    cell.innerHTML = '';
     this.pubsub.publish('/gfx/item/removed', obj);
     return this;
   };
-  Gfx.prototype.removePlayer = function(obj){
-  	var item = document.querySelector('.rpg-item[data-x="' + obj.x + '"][data-y="' + obj.y + '"]');
-  	if(item){
-  		item.parentNode.removeChild(item);
-  	}
+  Gfx.prototype.removePlayer = function(obj) {
+    var item = this.findEntity(obj);
+    if (item) {
+      item.parentNode.removeChild(item);
+    }
+  };
+  Gfx.prototype.findEntity = function(position){
+    return this.boardContainer.querySelector(this.entityTag+'[data-x="' + position.x + '"][data-y="' + position.y + '"]');
+  };
+  Gfx.prototype.findCell = function(position){
+    return this.boardContainer.querySelector('td[data-x="' + position.x + '"][data-y="' + position.y + '"]');
   };
   Gfx.prototype.hit = function(amout) {
     this.selectedEnemy.hit(amout);
@@ -334,19 +303,5 @@ RPG.module('Gfx', function() {
       life: life
     });
   };
-  // Gfx.prototype.getItem = function(obj, type) {
-  //   type = type || '';
-  //   return this.boardContainer.querySelector(type + '[data-x="' + obj.x + '"][data-y="' + obj.y + '"]');
-  // };
-  // Gfx.prototype.getPlayer = function(obj) {
-  //   return this.boardContainer.querySelector(this.itemElement+'[data-x="' + obj.x + '"][data-y="' + obj.y + '"]');
-  // };
-  // Gfx.prototype.publish = function() {
-  //   return this.pubsub.publish.apply(this, arguments);
-  // };
-  // Gfx.prototype.subscribe = function() {
-  //   return this.pubsub.subscribe.apply(this, arguments);
-  // };
   return Gfx;
-
 });
