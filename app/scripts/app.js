8e194cf272b407449956ab454c90722b9096fd48
Wassim CHEGHAM <wchegham@sii.fr>
2015-03-14 11:30:30 +0100

    clean dead code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,25 +6,12 @@
  * @author Wassim Chegham
  */
 RPG.run(function() {
-
   'use strict';
-
-
+  
   var socket = RPG.Factory.transport();
   socket.initialize();
-  
+
   var gfx = RPG.Factory.gfx();
   gfx.loop();
-
-  // var enemy = null;
-  // var nbEnemy = 0;
-  // for (var i = nbEnemy - 1; i >= 0; i--) {
-  //   enemy = RPG.Factory.enemy();
-  //   enemy.setLife(Math.random() * 100);
-  //   game.addEnemy(enemy);
-  // };
-  
-  // game.bots(true);
-  // game.play();
 
 });
