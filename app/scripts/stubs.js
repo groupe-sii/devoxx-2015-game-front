527cade49d226392712fbeca2ef5845b89df37a7
Wassim CHEGHAM <wchegham@sii.fr>
2015-03-14 11:32:04 +0100

    added a "stubs" file to simulation topics publishes

---
+++ app/scripts/stubs.js
@@ -0,0 +1,26 @@
+/* jshint devel:true */
+/* globals RPG */
+/**
+ * The main game loop
+ * @type {Function}
+ * @author Wassim Chegham
+ */
+RPG.run(function() {
+  'use strict';
+  var ps = RPG.Factory.pubsub();
+  setTimeout(function() {
+    ps.publish(RPG.topics.SUB_PLAYER_DIED, {
+      "@c": "string",
+      "id": "string",
+      "life": {
+        "current": 0,
+        "max": 0
+      },
+      "playerInfo": {
+        "name": "string",
+        "avatar": "string"
+      },
+      "states": "string"
+    });
+  }, 5000);
+});
